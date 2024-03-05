4844d8a469bed167456ae90d1ea9a6fb5f2d5593
shobhit-outscal <shobhit.rawat@outscal.com>
2024-03-05 21:29:50 +0530

    Added bullet pool

---
+++ Assets/Scripts/Bullets/BulletPool.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmicCuration.Bullets
+{
+    public class BulletPool
+    {
+        private BulletView bulletView;
+        private BulletScriptableObject bulletScriptableObject;
+        private List<PooledBullet> pooledBullets = new List<PooledBullet>();
+        public BulletPool(BulletView bulletView, BulletScriptableObject bulletScriptableObject)
+        {
+            this.bulletView = bulletView;
+            this.bulletScriptableObject = bulletScriptableObject;
+        }
+        public class PooledBullet
+        {
+            public BulletController Bullet;
+            public bool isUsed;
+        }
+    }
+}
