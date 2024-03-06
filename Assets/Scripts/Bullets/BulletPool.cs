2b8af4fa6d3bde16e2e020b343b35773f6c4f5b9
shobhit-outscal <shobhit.rawat@outscal.com>
2024-03-06 13:39:27 +0530

    Implemented generic object pool for bulletpool

--- Assets/Scripts/Bullets/BulletPool.cs
+++ Assets/Scripts/Bullets/BulletPool.cs
@@ -1,54 +1,24 @@
+using CosmicCuration.Utilities;
 using System.Collections.Generic;
 
 namespace CosmicCuration.Bullets
 {
-    public class BulletPool
+    public class BulletPool : GenericObjectPool<BulletController>
     {
         private BulletView bulletPrefab;
         private BulletScriptableObject bulletSO;
-        private List<PooledBullet> pooledBullets = new List<PooledBullet>();
 
         public BulletPool(BulletView bulletPrefab, BulletScriptableObject bulletSO)
         {
             this.bulletPrefab = bulletPrefab;
             this.bulletSO = bulletSO;
         }
-
-        public BulletController GetBullet()
+        protected override BulletController CreateItem()
         {
-            if (pooledBullets.Count > 0)
-            {
-                PooledBullet item = pooledBullets.Find(item => !item.isUsed);
-                if (item != null)
-                {
-                    item.isUsed = true;
-                    return item.Bullet;
-                }
-            }
-            return CreateNewPooledBullet();
+            return new BulletController(bulletPrefab, bulletSO);
         }
+        public BulletController GetBullet() => GetItem();
+      
 
-        private BulletController CreateNewPooledBullet()
-        {
-            PooledBullet newBullet = new PooledBullet();
-            newBullet.Bullet = CreateBullet();
-            newBullet.isUsed = true;
-            pooledBullets.Add(newBullet);
-            return newBullet.Bullet;
-        }
-
-        private BulletController CreateBullet() => new BulletController(bulletPrefab, bulletSO);
-
-        public void ReturnBullet(BulletController bullet)
-        {
-            PooledBullet pooledBullet = pooledBullets.Find(i => i.Bullet.Equals(bullet));
-            pooledBullet.isUsed = false;
-        }
-
-        public class PooledBullet
-        {
-            public BulletController Bullet;
-            public bool isUsed;
-        }
     }
 }
