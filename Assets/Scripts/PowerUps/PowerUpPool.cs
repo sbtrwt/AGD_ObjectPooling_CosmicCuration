3dd4314a0b878db10e74bc04efc844e0ec25e7dc
shobhit-outscal <shobhit.rawat@outscal.com>
2024-03-06 16:09:25 +0530

    Power up pool added

---
+++ Assets/Scripts/PowerUps/PowerUpPool.cs
@@ -0,0 +1,27 @@
+﻿using CosmicCuration.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmicCuration.PowerUps
+{
+    public class PowerUpPool : GenericObjectPool<PowerUpController>
+    {
+        private PowerUpData powerUpData;
+
+        public PowerUpController GetPowerUP<T>(PowerUpData powerUpData) where T : PowerUpController
+        {
+            this.powerUpData = powerUpData;
+            return GetItem<T>();
+        }
+        protected override PowerUpController CreateItem<T>()
+        {
+            if(typeof(T) == typeof(Shield)) { return new Shield(powerUpData); }
+            else if (typeof(T) == typeof(RapidFire)) { return new RapidFire(powerUpData); }
+            else if (typeof(T) == typeof(DoubleTurret)) { return new DoubleTurret(powerUpData); }
+            else { throw new NotSupportedException("PowerUp type not supported"); }
+        }
+    }
+}
