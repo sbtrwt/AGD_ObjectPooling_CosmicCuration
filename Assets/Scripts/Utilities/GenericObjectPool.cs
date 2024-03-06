bf86913b480772b1ac738563cbb762c51416c306
shobhit-outscal <shobhit.rawat@outscal.com>
2024-03-06 15:58:26 +0530

    Sub type added

--- Assets/Scripts/Utilities/GenericObjectPool.cs
+++ Assets/Scripts/Utilities/GenericObjectPool.cs
@@ -11,11 +11,11 @@ namespace CosmicCuration.Utilities
     {
         public List<PooledItem<T>> pooledItems = new List<PooledItem<T>>();
 
-        public virtual T GetItem()
+        public virtual T GetItem<U>() where U :T
         {
             if (pooledItems.Count > 0)
             {
-                PooledItem<T> item = pooledItems.Find(item => !item.isUsed);
+                PooledItem<T> item = pooledItems.Find(item => !item.isUsed && item.Item is U);
                 if (item != null)
                 {
                     item.isUsed = true;
@@ -25,16 +25,16 @@ namespace CosmicCuration.Utilities
             return CreateNewPooledItem();
         }
 
-        private T CreateNewPooledItem()
+        private T CreateNewPooledItem<U>() where U : T
         {
             PooledItem<T> newItem = new PooledItem<T>();
-            newItem.Item = CreateItem();
+            newItem.Item = CreateItem<U>();
             newItem.isUsed = true;
             pooledItems.Add(newItem);
             return newItem.Item;
         }
 
-        protected virtual T CreateItem()
+        protected virtual T CreateItem<U>() where U :T
         {
             throw new NotImplementedException("CreateItem() method not implemented in derived class");
         }
